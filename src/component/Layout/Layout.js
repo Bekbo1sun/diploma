92af0e5a0b48fd199e74d1409164525a591cd68e
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-04-07 14:29:39 +0600

    added a component to the navigation so that the logo is on the same line

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,4 +1,3 @@
-import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
 import "./Layout.css";
 
@@ -7,7 +6,6 @@ function Layout(props) {
     <div className="Layout">
      <header>
         <Nav />
-        <Logo />
      </header>
      <aside>
       <nav>NAV NOVIGATION</nav>
