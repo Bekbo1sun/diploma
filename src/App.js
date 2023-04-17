dbf212f0819c05e9d3010d0d0ebf078db805cb8f
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-04-17 15:57:53 +0600

    in the basket function added increase and decrease and I next the amount changes from the number of goods and added delete the goods

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ export default function App(){
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="cart" element={<Cart />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/delivery" element={<Delivery />} />
---
+++ src/component/CartList/CartList.js
@@ -0,0 +1,51 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AppContext } from "../../App";
+import "./CartList.css";
+
+export default function CartList() {
+  // получить список товаров и корзину
+  const { products, cart, setCart } = useContext(AppContext);
+
+  function onQuantityChange( product, qty ) {
+    setCart({
+      ...cart,
+      [product.id]: qty,
+    })
+  }
+
+  function onItemRemove( product ) {
+    const newCart = {...cart };
+    delete newCart[product.id];
+    setCart(newCart);
+  }
+
+  // получить только ID товаров в корзинке
+  const productIds = Object.keys(cart);
+
+  // чтобы вывести пройтись по товаром
+  const output = products
+  // удалить все товары которые не в корзине
+    .filter((product) => productIds.includes(product.id))
+    // вывести товары и их количество
+    .map((product) => (
+      <div className="CartItem" key={product.id}>
+        <img src={product.picture} alt={product.name} />
+        <Link to={"/product/" + product.slug}>{product.name}</Link>
+        {cart[product.id]}
+        <input 
+          type="number" 
+          value={cart[product.id]} 
+          min={1} 
+          onChange={(event) => onQuantityChange(product, +event.target.value)} />
+        <span>{cart[product.id] * product.price} som</span>
+        <button onClick={() => onItemRemove(product)}>Remove</button>
+      </div>
+    ));
+
+  return (
+    <div className="CartList">
+      {output}
+    </div>
+  );
+}
