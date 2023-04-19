372c47cfa26d23986c0a3abd6478610448309ea0
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-04-19 14:38:39 +0600

    связал это в firebase чтобы данные отправлялись а не авто обновлялся

--- src/component/pages/Cart.js
+++ src/component/pages/Cart.js
@@ -1,10 +1,12 @@
 import CartList from "../CartList/CartList";
+import OrderForm from "../OrderForm/OrderForm";
 
 export default function Cart() {
   return (
     <div className="Cart">
       <h1>Cart</h1>
       <CartList />
+      <OrderForm />
     </div>
   );
 }
