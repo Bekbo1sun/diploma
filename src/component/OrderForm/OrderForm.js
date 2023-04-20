faa6075d12fbbb4d1bffe1670fcba3c294e3e3f9
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-04-20 23:27:31 +0600

    created a code where you can write code and look at this code by clicking on the button

--- src/component/OrderForm/OrderForm.js
+++ src/component/OrderForm/OrderForm.js
@@ -1,13 +1,14 @@
 import { addDoc } from "firebase/firestore/lite";
 import "./OrderForm.css";
 import { ordersCollection } from "../../firebase";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../App";
 import { useNavigate } from "react-router";
 
 export default function OrderForm() {
   const { cart, setCart } = useContext(AppContext);
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false); // добавлено состояние
 
   if (Object.keys(cart).length === 0) {
     return "Your cart is empty.";
@@ -22,6 +23,7 @@ export default function OrderForm() {
       name: formData.get('name'),
       phone: formData.get('phone'),
       email: formData.get('email'),
+      password: formData.get('password'),
       address: formData.get('address'),
       cart: cart,
     })
@@ -29,6 +31,11 @@ export default function OrderForm() {
       setCart({});
       navigate('/thank-you');
     })
+  }
+
+  // удалена строка с вызовом querySelector
+  function togglePasswordVisibility() { // добавлена функция обработки клика на кнопке "Show Password"
+    setShowPassword(!showPassword);
   }
 
   return (
@@ -42,6 +49,12 @@ export default function OrderForm() {
       </label>
       <label>
         Email: <input type="email" name="email" required />
+      </label>
+      <label>
+        Password: <input type={showPassword ? "text" : "password"} name="password" required />
+        <button type="button" className="show-hide-password-button" onClick={togglePasswordVisibility}>
+          {showPassword ? 'Hide Password' : 'Show Password'}
+        </button>
       </label>
       <label>
         Address: <input type="text" name="address" required />
