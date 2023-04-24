937f90b2bf27579dfea29974dfaaa209daca3a55
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-04-24 15:52:00 +0600

    create the Orders

---
+++ src/component/pages/Orders.js
@@ -0,0 +1,10 @@
+import OrderList from "../OrderList/OrderList";
+
+export default function Orders() {
+  return (
+    <div className="Orders">
+      <h1>Orders</h1>
+      <OrderList />
+    </div>
+  )
+}
