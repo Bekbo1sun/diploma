a0c5ca768af84cdda83cfdbb88d22f0bfa94b309
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-05-16 00:50:17 +0600

    connected footer to layout

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,6 +1,7 @@
 import Auth from "../Auth/Auth";
 import CartLink from "../CartLink/CartLink";
 import CategoryList from "../CategoryList/CategoryList";
+import Footer from "../Footer/Footer";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
 import "./Layout.css";
@@ -19,7 +20,7 @@ export default function Layout(props) {
       </aside>
       <main>{props.children}</main>
       <footer>
-        Footer
+        <Footer />
       </footer>
     </div>
   );
