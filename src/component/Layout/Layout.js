52474c88f1a113ca793aa730220e28f3bf1fef59
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-05-18 14:11:16 +0600

    in layout connected dropdown and navtoggle

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import Auth from "../Auth/Auth";
 import CartLink from "../CartLink/CartLink";
 import CategoryList from "../CategoryList/CategoryList";
+import Drawer from "../Drawer/Drawer";
 import Footer from "../Footer/Footer";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+import NavToggle from "../NavToggle/NavToggle";
 import "./Layout.css";
 
 export default function Layout(props) {
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
+
   return (
     <div className="Layout">
       <header>
         <Logo />
         <Nav />
-        <Auth />
-        <CartLink />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
+        <ul>
+          <NavToggle callback={toggleDrawer} />
+          <Auth />
+          <CartLink />
+        </ul>
       </header>
       <aside>
         <CategoryList />
