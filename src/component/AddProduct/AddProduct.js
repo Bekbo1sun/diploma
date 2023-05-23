87d2368e094d4ac44419856209b1d29233e193b0
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-05-24 01:49:05 +0600

    corrected the text in some places in the code and removed unnecessary indentation, then included the AddProduct.css file

--- src/component/AddProduct/AddProduct.js
+++ src/component/AddProduct/AddProduct.js
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { AppContext } from "../../App";
 import { productsCollection, uploadProductPhoto } from "../../firebase";
 import { addDoc } from "firebase/firestore";
+import "./AddProduct.css";
 
 export default function AddProduct({ category }) {
   const { user } = useContext(AppContext);
@@ -16,9 +17,11 @@ export default function AddProduct({ category }) {
   function onChangeName(event) {
     setName(event.target.value);
   }
+
   function onChangePrice(event) {
     setPrice(event.target.value);
   }
+
   function onChangePicture(event) {
     const file = event.target.files[0];
     setPicture(file);
@@ -28,7 +31,7 @@ export default function AddProduct({ category }) {
     event.preventDefault();
 
     if (!picture) {
-      alert("Please upload an picture");
+      alert("Please upload a picture");
       return;
     }
 
@@ -56,36 +59,39 @@ export default function AddProduct({ category }) {
     <div className="AddProduct">
       <form onSubmit={onFormSubmit}>
         <h3>Create a new product</h3>
-        <label>
-          Name:
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
           <input
             type="text"
+            id="name"
             value={name}
             name="name"
             onChange={onChangeName}
             required
           />
-        </label>
-        <label>
-          Price:
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">Price:</label>
           <input
             type="number"
+            id="price"
             value={price}
             name="price"
             onChange={onChangePrice}
             min={0}
             required
           />
-        </label>
-        <label>
-          Picture:
+        </div>
+        <div className="form-group">
+          <label htmlFor="picture">Picture:</label>
           <input
             type="file"
+            id="picture"
             name="picture"
             onChange={onChangePicture}
             required
           />
-        </label>
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
