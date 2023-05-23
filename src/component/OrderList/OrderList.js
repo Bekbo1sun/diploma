5560545ad894ae05dbcaf32edb44997e89df4770
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-05-24 01:43:08 +0600

    orders section added to the footer

--- src/component/OrderList/OrderList.js
+++ src/component/OrderList/OrderList.js
@@ -9,7 +9,7 @@ export default function OrderList() {
     return "No orders found";
   }
 
-  // Выводим все заказыю
+  // Выводим все заказы
   const output = orders.map((order) => {
     // Вывести содержимое корзины для этого заказа
     const cartOutput = Object.keys(order.cart).map((productId) => {
@@ -20,20 +20,31 @@ export default function OrderList() {
       }
 
       return (
-        <div key={product.id}>
+        <div key={product.id} className="CartCard">
           <img src={product.picture} alt={product.name} />
-          {product.name}: {order.cart[productId]} X {product.price} som ={" "}
-          {order.cart[productId] * product.price} som
+          <div className="CartCard-details">
+            <div>{product.name}</div>
+            <div>
+              Quantity: {order.cart[productId]} x Price: {product.price} som ={" "}
+              {order.cart[productId] * product.price} som
+            </div>
+          </div>
         </div>
       );
     });
 
     return (
-      <div key={order.id} className="Order">
-        <div><strong> Name: </strong>{order.name}</div>
-        <div><strong>Phone: </strong>{order.phone}</div>
-        <div><strong>Address: </strong>{order.address}</div>
-        <div><strong>Cart: </strong>{cartOutput}</div>
+      <div key={order.id} className="OrderCard">
+        <div>
+          <strong>Name:</strong> {order.name}
+        </div>
+        <div>
+          <strong>Phone:</strong> {order.phone}
+        </div>
+        <div>
+          <strong>Address:</strong> {order.address}
+        </div>
+        <div className="OrderCard-cart">{cartOutput}</div>
       </div>
     );
   });
