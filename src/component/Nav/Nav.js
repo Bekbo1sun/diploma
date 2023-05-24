9659536430a71938a5b4fe3160400858a7eb1606
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-05-24 20:32:14 +0600

    Nav.js file formatted code

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -6,18 +6,26 @@ export default function Nav() {
     <nav className="Nav">
       <ul className="NavUl">
         <li className="NavItem">
-          <NavLink to="/" className="NavLink">Home</NavLink>
+          <NavLink to="/" className="NavLink">
+            Home
+          </NavLink>
         </li>
         <li className="NavItem">
-          <NavLink to="/about" className="NavLink">About</NavLink>
+          <NavLink to="/about" className="NavLink">
+            About
+          </NavLink>
         </li>
         <li className="NavItem">
-          <NavLink to="/orders" className="NavLink">Orders</NavLink>
+          <NavLink to="/orders" className="NavLink">
+            Orders
+          </NavLink>
         </li>
         <li className="NavItem">
-          <NavLink to="/support" className="NavLink">Support</NavLink>
+          <NavLink to="/support" className="NavLink">
+            Support
+          </NavLink>
         </li>
       </ul>
     </nav>
-  )
-};
+  );
+}
