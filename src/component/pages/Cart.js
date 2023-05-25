5ac712a007f9e38c16e1c9f6570e659564490614
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-05-25 17:07:13 +0600

    swapped tags

--- src/component/pages/Cart.js
+++ src/component/pages/Cart.js
@@ -6,10 +6,10 @@ export default function Cart() {
   return (
     <div className="Cart">
       <h1>Cart</h1>
+      <CartList />
       <div className="OrderForm">
         <OrderForm />
       </div>
-      <CartList />
     </div>
   );
 }
