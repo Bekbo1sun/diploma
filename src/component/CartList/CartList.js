ff600b902ce646fb4a2506d765f34134db9861c3
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2023-05-25 13:39:41 +0600

    added a change in the file cartlist.js

--- src/component/CartList/CartList.js
+++ src/component/CartList/CartList.js
@@ -4,13 +4,13 @@ import { AppContext } from "../../App";
 import "./CartList.css";
 
 export default function CartList() {
-  // получить список товаров и корзину
   const { products, cart, setCart } = useContext(AppContext);
 
   function onQuantityChange(product, qty) {
+    const newQty = Math.max(qty, 1); // Ensure quantity is not less than 1
     setCart({
       ...cart,
-      [product.id]: qty,
+      [product.id]: newQty,
     });
   }
 
@@ -20,14 +20,10 @@ export default function CartList() {
     setCart(newCart);
   }
 
-  // получить только ID товаров в корзинке
   const productIds = Object.keys(cart);
 
-  // чтобы вывести пройтись по товаром
   const output = products
-    // удалить все товары которые не в корзине
     .filter((product) => productIds.includes(product.id))
-    // вывести товары и их количество
     .map((product) => (
       <div className="CartItem" key={product.id}>
         <img src={product.picture} alt={product.name} />
@@ -35,16 +31,28 @@ export default function CartList() {
           <Link to={"/product/" + product.slug}>{product.name}</Link>
           <span>{cart[product.id] * product.price} som</span>
           <div className="Adition">
-            <button className="button-minus" onClick={() => onQuantityChange(product, cart[product.id] - 1)}>-</button>
+            <button
+              className="button-minus"
+              onClick={() => onQuantityChange(product, cart[product.id] - 1)}
+            >
+              -
+            </button>
             <input
               type="number"
               value={cart[product.id]}
               min={1}
               onChange={(event) => onQuantityChange(product, +event.target.value)}
             />
-            <button className="button-plus" onClick={() => onQuantityChange(product, cart[product.id] + 1)}>+</button>
+            <button
+              className="button-plus"
+              onClick={() => onQuantityChange(product, cart[product.id] + 1)}
+            >
+              +
+            </button>
           </div>
-          <button className="Remove" onClick={() => onItemRemove(product)}>Remove</button>
+          <button className="Remove" onClick={() => onItemRemove(product)}>
+            Remove
+          </button>
         </div>
       </div>
     ));
