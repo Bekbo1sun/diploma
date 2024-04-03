11c94063bc6a6b438bc76d31687e8061e314bba8
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2024-04-03 11:31:16 +0600

    added a new feature

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,10 +1,36 @@
+import React from 'react';
+import ProductCategory from './ProductCategory'; // Путь к вашему компоненту категории продуктов
 import "./Home.css";
 
-export default function Home() {
+function Home() {
+  const categories = [
+    {
+      name: 'Electronics',
+      products: [
+        { id: 1, name: 'Smartphone', image: 'phone.jpg', description: '...', price: '$500' },
+        { id: 2, name: 'Laptop', image: 'laptop.jpg', description: '...', price: '$1000' },
+      ],
+    },
+    {
+      name: 'Clothing',
+      products: [
+        { id: 3, name: 'T-Shirt', image: 'shirt.jpg', description: '...', price: '$20' },
+        { id: 4, name: 'Jeans', image: 'jeans.jpg', description: '...', price: '$50' },
+      ],
+    },
+    // Другие категории продуктов
+  ];
+
   return (
-    <div className="Home">
-      <h1>Home</h1>
-      <p>This is my home page</p>
+    <div className="home-page">
+      <h1>Welcome to Markom Store!</h1>
+      <div className="categories">
+        {categories.map((category) => (
+          <ProductCategory key={category.name} category={category} />
+        ))}
+      </div>
     </div>
   );
 }
+
+export default Home;
