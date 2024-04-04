1cf9e9889fcf9faca2d888eb09f39b921f071206
Bekbolsun <bekbolsunzarmamatov@gmail.com>
2024-04-04 09:03:24 +0600

    added links and removed description

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -5,17 +5,17 @@ import "./Home.css";
 function Home() {
   const categories = [
     {
-      name: 'Electronics',
+      name: 'Dried fruits',
       products: [
-        { id: 1, name: 'Smartphone', image: 'https://i1.stat01.com/2/3752/137515038/afacdb/kuraga-quot-korolevskaya-quot-vysshij-sort.jpg', description: '...', price: '500som' },
-        { id: 2, name: 'Laptop', image: 'https://firebasestorage.googleapis.com/v0/b/diplom-597c7.appspot.com/o/products%2F072facdc-36f4-4b3a-842d-9a3558da8498.jpg?alt=media&token=2cb390aa-d488-490a-962d-ab7073efe2ce', description: '...', price: '1000som' },
+        { id: 1, name: 'Dried apricots', image: 'https://i1.stat01.com/2/3752/137515038/afacdb/kuraga-quot-korolevskaya-quot-vysshij-sort.jpg', price: '108som' },
+        { id: 2, name: 'Figs', image: 'https://firebasestorage.googleapis.com/v0/b/diplom-597c7.appspot.com/o/products%2F157bf8c7-130d-4ff7-a128-5d387d8fce3f.jpg?alt=media&token=31b0aea8-99d2-4675-bf61-42e807b08dc8', price: '148som' },
       ],
     },
     {
-      name: 'Clothing',
+      name: 'Fruits',
       products: [
-        { id: 3, name: 'T-Shirt', image: 'https://firebasestorage.googleapis.com/v0/b/diplom-597c7.appspot.com/o/products%2F07034e7a-0ffc-4d0d-b868-a6ba399f6514.jpg?alt=media&token=086a0e48-9795-4488-9732-4c7105d8d517', description: '...', price: '20som' },
-        { id: 4, name: 'Jeans', image: 'https://firebasestorage.googleapis.com/v0/b/diplom-597c7.appspot.com/o/products%2F9f19b3d4-6b2b-40b7-a27f-b78bd03d7129.jpg?alt=media&token=0511630a-0173-4792-b166-544d6732377b', description: '...', price: '50 som' },
+        { id: 3, name: 'Watermelon', image: 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/11/watermelon-1296x728-header-1.jpg?w=1155&h=1528', price: '212som' },
+        { id: 4, name: 'Mandarin', image: 'https://soap-formula.ru/assets/images/products/261/532x315/mandarin.jpg', price: '40 som' },
       ],
     },
     // Другие категории продуктов
